5919d2fc4f225f08921ef7179b083a99456f06bf
Kushagra <kushagragupta6038@gmail.com>
2020-11-06 15:08:53 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+// const { response } = require("express");
+
+console.log('Client Side javascript is loaded');
+
+
+
+const weatherForm = document.querySelector('form');
+
+const search = document.querySelector('input');
+
+const messageOne = document.querySelector('#message-1')
+
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'from javascript';
+
+weatherForm.addEventListener('submit',(event)=>{
+   event.preventDefault();  //Used For not refresh the window after pressing button 
+
+   const location = search.value;
+
+   messageOne.textContent = 'Loding...';
+   messageTwo.textContent = '';
+   
+
+   fetch(`http://localhost:4000/weather?address=${location}`).then((response)=>{
+    response.json().then((data) =>{
+        if(data.error){
+            messageOne.textContent = data.error;
+            // console.log(data.error);
+        } else{
+            messageOne.textContent = 'Location = ' + data.location
+            messageTwo.textContent = 'Forcast = '  + 'It is mostly '+ JSON.stringify(data.forcast.weather_decreption)+' and temperature is about '
+            + JSON.stringify(data.forcast.temperature)+ ' Degree Celcius & FellsLike Temperature is ' + JSON.stringify(data.forcast.feelsLikeTemp)+' Degree Celcius'
+            // console.log(data.location);
+            // console.log(data.forcast);
+        }
+    })
+})
+
+})
